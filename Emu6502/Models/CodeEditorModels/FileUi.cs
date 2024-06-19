2c83fa77a901b99e4b3a9b02bc2a7aabc063907f
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-19 22:07:51 +0300

    Added the FileUi model

---
+++ Emu6502/Models/CodeEditorModels/FileUi.cs
@@ -0,0 +1,27 @@
+﻿using Emu6502.Services.BaseModels;
+
+namespace Emu6502.Models.CodeEditorModels;
+
+public class FileUi : BaseUiModel
+{
+    private string _fileName = string.Empty;
+    public string Filename
+    {
+        get => _fileName;
+        set => SetProperty(ref _fileName, value);
+    }
+
+    private string _filePath = string.Empty;
+    public string FilePath
+    {
+        get => _filePath;
+        set => SetProperty(ref _filePath, value);
+    }
+
+    private string _contents = string.Empty;
+    public string Contents
+    {
+        get => _contents;
+        set => SetProperty(ref _contents, value);
+    }
+}
