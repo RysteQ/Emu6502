0179942524fe8a99693402cd145251b7d9cbd435
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-19 22:07:43 +0300

    Added the base models

---
+++ Emu6502/Services/BaseModels/BaseUiModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Emu6502.Services.BaseModels;
+
+public class BaseUiModel
+{
+    public void SetProperty<T>(ref T source, T value, [CallerMemberName] string propertyName = "")
+    {
+        source = value;
+
+        if (ObservingChanges)
+            ValueChanged = true;
+
+        OnPropertyChanged(propertyName);
+    }
+
+    public void StartObservingForPropertyChanges()
+    {
+        ObservingChanges = true;
+    }
+
+    public void StopObservingForPropertyChanges()
+    {
+        ObservingChanges = false;
+    }
+
+    public bool IsObservingChanges()
+    {
+        return ObservingChanges;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new(propertyName));
+
+    public bool ValueChanged { get; private set; }
+    public bool ObservingChanges { get; private set; }
+}
---
+++ Emu6502/Services/BaseModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Emu6502.Services.BaseModels;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    public void SetProperty<T>(ref T source, T value, [CallerMemberName] string propertyName = "")
+    {
+        source = value;
+        OnPropertyChanged(propertyName);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new(propertyName));
+
+    private bool _loading;
+    public bool Loading
+    {
+        get => _loading;
+        set => SetProperty(ref _loading, value);
+    }
+
+    private bool _subordinateLoading;
+    public bool SubordinateLoading
+    {
+        get => _subordinateLoading;
+        set => SetProperty(ref _subordinateLoading, value);
+    }
+
+    public bool InternalLoading { get; set; }
+}
