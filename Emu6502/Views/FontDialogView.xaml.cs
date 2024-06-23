1ccf90d2100dd8994e0690d8205b9dcfec1018fc
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-23 18:54:45 +0300

    Added a font dialog window, just the window, nothing else

---
+++ Emu6502/Views/FontDialogView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Emu6502.Views;
+
+/// <summary>
+/// Interaction logic for FontDialogView.xaml
+/// </summary>
+public partial class FontDialogView : Window
+{
+    public FontDialogView()
+    {
+        InitializeComponent();
+    }
+}
