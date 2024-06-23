06063ddec3bec79eb52f11890d4424584da0bb3f
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-23 18:54:56 +0300

    Added a font ui model

---
+++ Emu6502/Models/CodeEditorModels/FontUi.cs
@@ -0,0 +1,21 @@
+﻿using Emu6502.Services.BaseModels;
+using System.Windows.Media;
+
+namespace Emu6502.Models.CodeEditorModels;
+
+public class FontUi : BaseUiModel
+{
+    private int _size = 10;
+    public int Size
+    {
+        get => _size;
+        set => SetProperty(ref _size, value);
+    }
+
+    private FontFamily _font = new("Courier");
+    public FontFamily Font
+    {
+        get => _font;
+        set => SetProperty(ref _font, value);
+    }
+}
