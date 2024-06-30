6e594a998a9bd60604e5404147993c685d243e19
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-30 22:43:07 +0300

    I made the code editor have row and column labels, I also added a file path label

--- Emu6502/Views/CodeEditorView.xaml.cs
+++ Emu6502/Views/CodeEditorView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Emu6502.ViewModels;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Emu6502.Views;
 
@@ -11,6 +13,14 @@ public partial class CodeEditorView : Window
 
         _viewModel = new();
         DataContext = _viewModel;
+    }
+
+    private void TextBoxCodeEditorTextChanged(object sender, TextChangedEventArgs e)
+    {
+        TextBox textBox = (TextBox)sender;
+
+        LabelRowCount.Content = $"Row: {textBox.Text.Take(textBox.CaretIndex).Count(selectedChar => selectedChar == '\n') + 1}";
+        LabelColumnCount.Content = $"Col: {textBox.Text.Split('\n')[textBox.Text.Take(textBox.CaretIndex).Count(selectedChar => selectedChar == '\n')].Length + 1}";
     }
 
     private CodeEditorVM _viewModel;
