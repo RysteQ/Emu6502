7574e9df5e477d37360aefd15c72e16417aab044
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-07-01 01:52:35 +0300

    Added the instruction enums and adressing mode enums

--- Emu6502/Enums/Emulator/AddressingModeEnum.cs
+++ Emu6502/Enums/Emulator/AddressingModeEnum.cs
@@ -2,4 +2,16 @@
 
 public enum AddressingModeEnum
 {
+    IMPLICIT,
+    ACCUMULATOR,
+    ZERO_PAGE,
+    ZERO_PAGE_Y,
+    ZERO_PAGE_X,
+    RELATIVE,
+    ABSOLUTE,
+    ABSOLUTE_X,
+    ABSOLUTE_Y,
+    INDIRECT,
+    INDEXED_INDIRECT,
+    INDIRECT_INDEXED
 }
