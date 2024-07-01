d8a13d8ef30729643a41133147074d5cb31489d5
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-07-01 05:43:25 +0300

    Added the instruction classes implementing the IInstruction interface

---
+++ Emu6502/Models/Emulator/Instructions/BeqInstruction.cs
@@ -0,0 +1,17 @@
+﻿using Emu6502.Enums.Emulator;
+using Emu6502.Interfaces.Emulator;
+
+namespace Emu6502.Models.Emulator.Instructions;
+
+public class BeqInstruction : IInstruction
+{
+    public void Execute(ref EmulatorMemory emulatorMemory, ref EmulatorFlags emulatorFlags)
+    {
+        throw new NotImplementedException();
+    }
+
+    public AddressingModeEnum AddressingMode { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+    public byte InstructionHexadecimal { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+    public int InstructionLength { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+    public int InstructionCycles { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+}
