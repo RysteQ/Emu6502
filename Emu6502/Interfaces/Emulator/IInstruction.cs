fcba39e08b915e7d88ce2a72b5615307ac11b43c
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-07-01 05:43:04 +0300

    Added the IInstruction interface

---
+++ Emu6502/Interfaces/Emulator/IInstruction.cs
@@ -0,0 +1,14 @@
+﻿using Emu6502.Enums.Emulator;
+using Emu6502.Models.Emulator;
+
+namespace Emu6502.Interfaces.Emulator;
+
+public interface IInstruction
+{
+    void Execute(ref EmulatorMemory emulatorMemory, ref EmulatorFlags emulatorFlags);
+
+    AddressingModeEnum AddressingMode { get; init; }
+    byte InstructionHexadecimal { get; init; }
+    int InstructionLength { get; init; }
+    int InstructionCycles { get; init; }
+}
